3287afc6f86d7fac9d60ed8bf80372c3e73d1cec
Reza <rezapapi138418@gmail.com>
2025-11-01 13:38:30 +0330

    finish phase2 in redis_project

--- app/core/cart/redis_cart.py
+++ app/core/cart/redis_cart.py
@@ -7,8 +7,15 @@ redis_client = settings.REDIS_CLIENT
 
 CART_TTL = 60 * 30
 
+def _refresh_cart_ttl(session_id):
+    cart_key = _cart_key(session_id)
+    promo_code = f"{cart_key}:promo_code"
+
+    redis_client.expire(cart_key, CART_TTL)
+    redis_client.expire(promo_code, CART_TTL)
+    
 def _cart_key(session_id):
-    return f'cart: {session_id}'
+    return f'cart:{session_id}'
 
 def add_to_cart(session_id, product_id, name, price, quantity):
     cart_key = _cart_key(session_id)
@@ -23,6 +30,7 @@ def add_to_cart(session_id, product_id, name, price, quantity):
     # Set the product data in the cart
     # cart:item:session_id
     redis_client.hset(cart_key, product_id, json.dumps(product_data))
+    _refresh_cart_ttl(session_id)
     
 # get_carts
 def get_cart(session_id):
@@ -38,6 +46,8 @@ def remove_cart(session_id, product_id):
     if redis_client.hlen(key) == 0:
         promo_key = f"cart:{session_id}:promo_code"
         redis_client.delete(promo_key)
+    
+    _refresh_cart_ttl(session_id)
     
 # remove all items in cart
 def remove_all_items(session_id):
@@ -56,6 +66,8 @@ def increment_quantity(session_id, product_id, step=1):
     data['quantity'] += step
     redis_client.hset(key, product_id, json.dumps(data))
     
+    _refresh_cart_ttl(session_id)
+    
     return True
 
 # decrement quantity
@@ -70,7 +82,9 @@ def decrement_quantity(session_id, product_id, step=1):
     data['quantity'] += max(data['quantity'] - step, 1)
     redis_client.hset(key, product_id, json.dumps(data))
     
-    return 
+    _refresh_cart_ttl(session_id)
+    
+    return True
 
 # set explicity
 def set_quantity(session_id, product_id, quantity):
@@ -84,11 +98,14 @@ def set_quantity(session_id, product_id, quantity):
     data['quantity'] = quantity
     redis_client.hset(key, product_id, json.dumps(data))
     
+    _refresh_cart_ttl(session_id)
+    
     return True
 
 def set_cart_promo_code(session_id, promo_code):
     key = f"cart:{session_id}:promo_code"
     redis_client.set(key, promo_code)
+    _refresh_cart_ttl(session_id)
 
 def get_cart_promo_code(session_id):
     key = f"cart:{session_id}:promo_code"
@@ -105,3 +122,4 @@ def update_cart_item(session_id, product_id, name, price, quantity):
     }
     
     redis_client.hset(key, product_id, json.dumps(product_data))
+    _refresh_cart_ttl(session_id)
